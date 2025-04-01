f2dd534715bf58bdc8a12c3454407f779a134b23
Al Fatih Muzakki <malfazakki@gmail.com>
2025-04-01 21:20:08 +0700

    feat(models): setup User, Post & Category relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,7 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
-    use HasFactory;
+class Category extends Model {
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function posts() {
+        return $this->belongsToMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,7 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
-{
-    use HasFactory;
+class Post extends Model {
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'content',
+        'image',
+        'is_published'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
 }
