020c229822b3a890699a5ebb53a3a3f3eda1ea62
Al Fatih Muzakki <malfazakki@gmail.com>
2025-04-02 10:53:21 +0700

    feat(controllers): implement category controllers

--- app/Http/Controllers/Admin/CategoryContoller.php
+++ app/Http/Controllers/Admin/CategoryContoller.php
@@ -3,63 +3,72 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
-class CategoryContoller extends Controller
-{
+class CategoryContoller extends Controller {
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
-        //
+    public function index() {
+        $categories = Category::withCount('posts')->latest()->paginate(10);
+        return view('admin.categories.index', compact('categories'));
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
+    public function create() {
+        return view('admin.categories.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
-        //
-    }
+    public function store(Request $request) {
+        $request->validate([
+            'name' => 'required|string|max:255|unique:categories'
+        ]);
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
+        Category::create([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name)
+        ]);
+
+        return redirect()->route('admin.categories.index')->with('success', 'Category created successfully');
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
-    {
-        //
+    public function edit(Category $category) {
+        return view('admin.category.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
-    {
-        //
+    public function update(Request $request, Category $category) {
+        $request->validate([
+            'name' => "required|string|max:255|unique:categories,name,{$category->id}",
+        ]);
+
+        $category->update([
+            'name' => $request->name,
+            'slug' => Str::slug($request->name)
+        ]);
+
+        return redirect()->route('admin.categories.index')->with('success', "Category updated successfully");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
-    {
-        //
+    public function destroy(Category $category) {
+        $category->post()->detach();
+        $category->delete();
+
+        return redirect()->route('admin.categories.index')->with('success', 'Category update successfully');
     }
 }
